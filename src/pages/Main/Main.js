a73593778eac5d06317a58775a6b58ddd37ae8aa
danielPantalena <pantalenadaniel@gmail.com>
2021-07-05 17:20:10 -0300

    Create wallAppApi

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Post, LoginContainer } from '../../components';
+import wallAppApi from '../../services/api';
 
 const Main = () => {
+  useEffect(() => {
+    wallAppApi.get('posts/').then(response => console.log(response));
+    return () => {};
+  }, []);
   return (
     <div>
       <LoginContainer />
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const storedToken = sessionStorage.token;
 
-const baseURL = 'http://localhost:3000';
+const baseURL = 'http://127.0.0.1:8000/';
 const timeout = 3000;
 const headers = {
   'Content-Type': 'application/json',
@@ -12,8 +12,11 @@ if (storedToken) headers.Authorization = `Token ${storedToken}`;
 
 const instance = axios.create({ baseURL, timeout, headers });
 
-const api = {
-  get: (url) => instance.get(url),
+const wallAppApi = {
+  get: (url, headers) => instance.get(url, { headers }),
+  post: (url, data, headers) => instance.post(url, data, { headers }),
+  put: (url, data, headers) => instance.put(url, data, { headers }),
+  delete: (url, headers) => instance.delete(url, { headers }),
 };
 
-export default api;
+export default wallAppApi;
