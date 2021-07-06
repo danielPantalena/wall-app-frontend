20f8b3f1c94efbc2f6e69c171fd0a2102bb8fd8c
danielPantalena <pantalenadaniel@gmail.com>
2021-07-06 18:05:52 -0300

    Start css

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { default as Button } from './Button/Button';
 export { default as Post } from './Post/Post';
 export { default as LoginContainer } from './LoginContainer/LoginContainer';
 export { default as PostForm } from './PostForm/PostForm';
-export { default as Header } from './Header/Header';
 export { default as PostsList } from './PostsList/PostsList';
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { Header, PostsList, PostForm, LoginContainer } from '../../components';
+import { PostsList, PostForm, LoginContainer } from '../../components';
+import './style.css';
 
 const Main = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(!!sessionStorage.userToken);
 
   return (
     <div>
-      <Header />
-      <LoginContainer isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <header className="flex-row">
+        <h1>Wall App</h1>
+        <LoginContainer isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      </header>
       {isLoggedIn && <PostForm />}
       <PostsList isLoggedIn={isLoggedIn} />
     </div>
